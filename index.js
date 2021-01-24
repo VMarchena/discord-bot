fa20536b1d62ed68fe29622b86301f656b59ee08
Vitor Marchena <vitor.marchena@stoneage.com.br>
2021-01-24 18:30:28 -0300

    Readind token from env file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 const { prefix } = require("./config.json");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
