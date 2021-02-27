7e3ef0fa2b507382351c290651298f7c675f68f8
Vitor Marchena <vitor.marchena@stoneage.com.br>
2021-02-26 21:47:11 -0300

    Sending gif on finish amongus command

--- src/commands/impostor.ts
+++ src/commands/impostor.ts
@@ -9,7 +9,10 @@ const impostor: Command = {
 
     if (channel) {
       const connection = await channel.join();
-      youtube.play(connection, "https://youtu.be/uG45me0ekts");
+      const onFinish = () => {
+        message.channel.send(`https://tenor.com/view/puppyy3533amoung-us-puppyy-kitchen-pantry-amoung-us-impostor-puppyy-amoung-us-impostor-gif-18651428`);
+      };
+      youtube.play(connection, "https://youtu.be/uG45me0ekts", onFinish);
     } else {
       message.reply("You need to join a voice channel first!");
     }
