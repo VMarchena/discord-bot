89e81a8181a7e17b94fd61edb66144225d6c1990
Erick Pires <rcckas@gmail.com>
2021-03-31 20:26:35 -0300

    Utilizando funções no arquivo principal

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,55 @@ import dotenv from "dotenv";
 import { Command } from "./ICommands";
 dotenv.config();
 
-const client = new Discord.Client();
-const commands = new Discord.Collection<string, Command>();
+function main() {
+  const client = new Discord.Client();
+  const commands = new Discord.Collection<string, Command>();
 
-const commandFiles = fs
-  .readdirSync("./src/commands")
-  .filter((file) => file.endsWith(".ts"));
+  readCommands(commands);
+  initializeClient(client);
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.set(command.name, command);
+  client.on("message", async (message) => {
+
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift()?.toLowerCase();
+
+    if (command && commands.has(command)) {
+      if (command == 'help') {
+        outputHelp(message, commands);
+      }
+
+      commands.get(command)?.execute(message, args);
+    }
+  });
 }
 
-function outputHelp(message: Discord.Message) {
+function readCommands(commands: Discord.Collection<string, Command>) {
+  const commandFiles = fs
+    .readdirSync("./src/commands")
+    .filter((file) => file.endsWith(".ts"));
+
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.set(command.name, command);
+  }
+}
+
+function initializeClient(client: Discord.Client) {
+  client.once("ready", () => {
+    console.log("Ready!");
+  });
+
+  if (!process.env.DISCORD_TOKEN) {
+    console.error('Failed to read DISCORD_TOKEN!');
+    process.exit(1);
+  }
+
+  client.login(process.env.DISCORD_TOKEN);
+}
+
+function outputHelp(message: Discord.Message, commands: Discord.Collection<string, Command>) {
   var helpMessage: string = `Hello <@${message.author.id}>. These are the available commands:\n`;
 
   commands.each((command: Command) => {
@@ -27,24 +63,4 @@ function outputHelp(message: Discord.Message) {
   message.channel.send(helpMessage);
 }
 
-client.once("ready", () => {
-  console.log("Ready!");
-});
-
-client.login(process.env.DISCORD_TOKEN);
-
-client.on("message", async (message) => {
-
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const command = args.shift()?.toLowerCase();
-
-  if (command && commands.has(command)) {
-    if (command == 'help') {
-      outputHelp(message);
-    }
-
-    commands.get(command)?.execute(message, args);
-  }
-});
+main();
